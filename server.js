34ec599a1700c629fc95a802a00534b9a155028c
User Name <jsalcedo19@gmail.com>
2024-11-26 11:54:57 -0500

    created signup page

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
+// loads environment variables "keeps Sensitive data"
 const dotenv = require("dotenv");
 dotenv.config();
+
+//imports modules
 const express = require("express");
 const mongoose = require("mongoose");
 
+//express app created
 const app = express();
+
+//monitors mongoDB connection
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+//import authcontroller 
+const authController = require("./controllers/auth.js");
+
+//middleware
+app.use("/auth", authController);
+
 
 
 // GET routes 
 app.get("/", async (req, res) => {
     res.send("hello, friend testing landing page!");
   });
+
   
-// server Connection
+  
+
+
+
+
+
+
+// server Connection "Starts Server"
 app.listen(3000, () => {
   console.log("Listening on port http://localhost:3000");
 });
