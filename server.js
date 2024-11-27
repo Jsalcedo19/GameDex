203ea8b1d95bc3f14127d9ed8f4532a47747d2ec
User Name <jsalcedo19@gmail.com>
2024-11-26 22:34:07 -0500

    updated auth files

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 // loads environment variables "keeps Sensitive data"
 const dotenv = require("dotenv");
 dotenv.config();
-
-//imports modules
 const express = require("express");
+const app = express();//express app created
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 
+
+//declare port
 const port = process.env.PORT ? process.env.PORT : "3000";
 
-//express app created
-const app = express();
-
 //monitors mongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+
 //import authcontroller 
 const authController = require("./controllers/auth.js");
 
+
+
 //middleware
+app.use(express.json());
 app.use("/auth", authController);
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
 
 
 // GET routes 
